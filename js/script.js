3d4c3f9fb629cd8c93b690cba357e24a865c7533
Himanshi Sharma <himanshi9may2000@gmail.com>
2021-06-17 22:55:59 +0530

    Fifth commit

--- js/script.js
+++ js/script.js
@@ -1,7 +1,44 @@
-function openForm() {
-  document.getElementById("myForm").style.display = "block";
-}
+/*fetch('category.json')
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                appendData(data);
+            })
+            .catch(function (err) {
+                console.log('error: ' + err);
+            });
+        function appendData(data) {
+            var mainContainer = document.getElementById("myData");
+            for (var i = 0; i < data.length; i++) {
+                var div = document.createElement("div");
+                div.innerHTML =   data[i].firstName;
 
-function closeForm() {
-  document.getElementById("myForm").style.display = "none";
-}
+
+
+                mainContainer.appendChild(div);
+            }
+        }
+        
+*/
+
+
+
+fetch('category.json')
+.then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                appendData(data);
+            })
+            .catch(function (err) {
+                console.log('error: ' + err);
+            });
+        function appendData(data) {
+             var div = document.createElement("div");
+             div.innerHTML = data[category].firstName;
+             mainContainer.appendChild(div);
+         }
+
+
+
